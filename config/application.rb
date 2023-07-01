7afaacf3f8bce376e1a0034dc9ee2f273e51deb6
Rodrigo Lira <rlirapizarro@gmail.com>
2023-07-01 05:20:02 -0400

    update bootstrap

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module BlockBuster
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.precompile += %w[application.bootstrap.scss]
+
   end
 end
